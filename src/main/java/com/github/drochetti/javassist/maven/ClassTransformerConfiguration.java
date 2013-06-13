7d3409913a1aa7a939d507b6e3a7b851c337f2be
barthel <barthel@users.noreply.github.com>
2013-06-13 19:42:46 +0200

    * configuration of class transformer added * <transformerClass> POM element extended

---
+++ src/main/java/com/github/drochetti/javassist/maven/ClassTransformerConfiguration.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2013 https://github.com/barthel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.drochetti.javassist.maven;
+
+import java.util.Properties;
+
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Configuration object for {@link ClassTransformer} class.
+ *
+ * @author Uwe Barthel
+ */
+public class ClassTransformerConfiguration {
+	@Parameter(property = "className", required = true)
+	private String className;
+	
+	@Parameter(property = "properties", required = false)
+	private Properties properties;
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+}
