217391e9fbd41a9599fddbe1df391f2302f41d7f
barthel <barthel@users.noreply.github.com>
2014-06-17 20:02:23 +0200

    reduce visibility of internal stampField #26

---
+++ src/test/java/de/icongmbh/oss/maven/plugin/javassist/ClassTransformerTest.java
@@ -0,0 +1,88 @@
+package de.icongmbh.oss.maven.plugin.javassist;
+
+import java.io.Serializable;
+
+import javassist.CannotCompileException;
+import javassist.CtClass;
+import javassist.CtField;
+import javassist.bytecode.AccessFlag;
+import javassist.bytecode.ClassFile;
+
+import org.easymock.Capture;
+import org.easymock.EasyMock;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ClassTransformerTest {
+
+	@Test
+	public void testModifierOnStampField_class() throws CannotCompileException {
+		final SampleTransformer sampleTransformer = new SampleTransformer();
+		final ClassFile candidateClassFile = new ClassFile(false, _ClassStub.class.getName(), null);
+		final CtClass candidateClassMock = EasyMock.createMock("candidateClassMock",CtClass.class);
+		EasyMock.expect(candidateClassMock.getClassFile2()).andReturn(candidateClassFile).anyTimes();
+		final Capture<CtField> fieldCapture = new Capture<CtField>();
+		candidateClassMock.addField(EasyMock.capture(fieldCapture));
+		EasyMock.expectLastCall().times(1);
+		EasyMock.expect(candidateClassMock.isInterface()).andReturn(Boolean.FALSE);
+		EasyMock.expect(candidateClassMock.isFrozen()).andReturn(Boolean.FALSE);
+
+		EasyMock.replay(candidateClassMock);
+		sampleTransformer.applyStamp(candidateClassMock );
+
+		EasyMock.verify(candidateClassMock);
+		Assert.assertThat(fieldCapture.getValue(), CoreMatchers.notNullValue());
+		final CtField ctField = fieldCapture.getValue();
+		Assert.assertThat(ctField.getModifiers(), CoreMatchers.equalTo(AccessFlag.PRIVATE | AccessFlag.STATIC | AccessFlag.FINAL));
+	}
+
+	@Test
+	public void testModifierOnStampField_interface() throws CannotCompileException {
+		final SampleTransformer sampleTransformer = new SampleTransformer();
+		final ClassFile candidateClassFile = new ClassFile(true, _InterfaceStub.class.getName(), null);
+		final CtClass candidateClassMock = EasyMock.createMock("candidateClassMock",CtClass.class);
+		EasyMock.expect(candidateClassMock.getClassFile2()).andReturn(candidateClassFile).anyTimes();
+		final Capture<CtField> fieldCapture = new Capture<CtField>();
+		candidateClassMock.addField(EasyMock.capture(fieldCapture));
+		EasyMock.expectLastCall().times(1);
+		EasyMock.expect(candidateClassMock.isInterface()).andReturn(Boolean.TRUE);
+		EasyMock.expect(candidateClassMock.isFrozen()).andReturn(Boolean.FALSE);
+
+		EasyMock.replay(candidateClassMock);
+		sampleTransformer.applyStamp(candidateClassMock );
+
+		EasyMock.verify(candidateClassMock);
+		Assert.assertThat(fieldCapture.getValue(), CoreMatchers.notNullValue());
+		final CtField ctField = fieldCapture.getValue();
+		Assert.assertThat(ctField.getModifiers(), CoreMatchers.equalTo(AccessFlag.PUBLIC | AccessFlag.STATIC | AccessFlag.FINAL));
+	}
+
+	private static class _ClassStub extends Number {
+
+		@Override
+		public int intValue() {
+			return 0;
+		}
+
+		@Override
+		public long longValue() {
+			return 0;
+		}
+
+		@Override
+		public float floatValue() {
+			return 0;
+		}
+
+		@Override
+		public double doubleValue() {
+			return 0;
+		}
+		
+	}
+	
+	private static interface _InterfaceStub extends Serializable {
+		// nothing
+	}
+}
